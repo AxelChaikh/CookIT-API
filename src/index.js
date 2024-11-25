43509e77d6415e7c715d7f7f6c27da5954873396
AxelChaikh <axelchaikh@gmail.com>
2024-11-25 08:49:53 -0300

    CORS Authorizattion update

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(express.json({
 app.use(cors({
     origin: '*', // Permitir solicitudes desde este origen
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Métodos HTTP permitidos
-    allowedHeaders: ['Content-Type', 'Authorization', 'role'], // Encabezados permitidos
+    allowedHeaders: ['Content-Type', 'role'], // Encabezados permitidos
     credentials: true
 }));
 
